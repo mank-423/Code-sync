4c23e6cef141642fc5e3dedd615690da361c0e59
Mayank Kumar <mk4664348@gmail.com>
2025-12-16 00:01:28 +0530

    Schedule and dashboard

--- src/app/(root)/schedule/page.tsx
+++ src/app/(root)/schedule/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client"
+
+import LoaderUI from "@/components/LoaderUI";
+import { useUserRole } from "@/hooks/useUserRole";
+import { useRouter } from "next/navigation"
+import InterviewScheduleUI from "./InterviewScheduleUI";
+
 
 const SchedulePage = () => {
-  return (
-    <div>SchedulePage</div>
-  )
+
+  const router = useRouter();
+
+  const { isInterviewer, isLoading } = useUserRole();
+
+  if (isLoading) return <LoaderUI />
+  if (!isInterviewer) return router.push('/');
+
+  return <InterviewScheduleUI />
 }
 
 export default SchedulePage
